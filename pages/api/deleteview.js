4e443102873be25d058bbcac4b58aa9ae216cf2c
arash <arash.asnaashari@gmail.com>
2021-02-24 14:06:23 +0330

    w1w

--- pages/api/deleteview.js
+++
@@ -1,9 +0,0 @@
-import View from "../../model/View";
-import dbConnect from "../../utils/dbConnect";
-
-export default async function handler(req, res) {
-  await dbConnect();
-  const deletedComment = await View.findByIdAndDelete(req.body.commentId);
-  await deletedComment;
-  res.status(200).json({ msg: true });
-}
