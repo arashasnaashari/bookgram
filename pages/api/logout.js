d4414306cd7db63dbad12028c96374ebe36c2133
arashasnaashari <arash.asnaashari83@gmail.com>
2021-03-26 15:50:05 +0430

    front

--- pages/api/logout.js
+++ pages/api/logout.js
@@ -1,6 +1,6 @@
-import withSession from '../../utils/session'
+import withSession from "../../utils/session";
 
 export default withSession(async (req, res) => {
-  req.session.destroy()
-  res.json({ isLoggedIn: false })
-})
+  await req.session.destroy();
+  res.json({ isLoggedIn: false });
+});
