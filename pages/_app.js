894a0a11f5b3e41b35886d0a0364ffd3cb57f677
arashasnaashari <arash.asnaashari83@gmail.com>
2021-03-27 17:14:22 +0430

    front3-pwa

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import "../styles/globals.css";
 import Contx from "../context/auth-context";
 import { useState, useEffect, useContext } from "react";
 import ReactNotification from "react-notifications-component";
+import Head from "next/head";
 
 import "react-notifications-component/dist/theme.css";
 
@@ -17,16 +18,46 @@ function MyApp({ Component, pageProps }) {
     useModel(false);
   };
   return (
-    <Contx.Provider
-      value={{
-        setModel: Modeling,
-        clozeModal: Clozing,
-        modal: model,
-      }}
-    >
-      <ReactNotification />
-      <Component {...pageProps} />
-    </Contx.Provider>
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+        <meta name="description" content="Description" />
+        <meta name="keywords" content="Keywords" />
+        <title>Next.js PWA Example</title>
+
+        <link rel="manifest" href="/manifest.json" />
+        <link
+          href="/icons/favicon-16x16.png"
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+        />
+        <link
+          href="/icons/favicon-32x32.png"
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+        />
+        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+        <meta name="theme-color" content="#317EFB" />
+      </Head>
+
+      <Contx.Provider
+        value={{
+          setModel: Modeling,
+          clozeModal: Clozing,
+          modal: model,
+        }}
+      >
+        <ReactNotification />
+        <Component {...pageProps} />
+      </Contx.Provider>
+    </>
   );
 }
 
