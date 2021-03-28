e35f7e3dc3908bb50a843438fa6332329cc0ceec
arashasnaashari <arash.asnaashari83@gmail.com>
2021-03-28 20:49:10 +0430

    front-error

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -35,7 +35,9 @@ export const getServerSideProps = withSession(async function ({ req, res }) {
   await dbConnect();
   const user = req.session.get("user");
   if (!user) {
-    res.redirect("https://bookgram.vercel.app/login");
+    res.setHeader("location", "/login");
+    res.statusCode = 302;
+    res.end();
   }
   const usero = await Reader.find({ userId: user.userId })
     .sort({ date: -1 })
