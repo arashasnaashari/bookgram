b68cbc3f82a25c76f3777c4b6369f74cae5a3edf
arash <arash.asnaashari@gmail.com>
2021-04-18 21:13:28 +0430

    crips

--- pages/_document.js
+++ pages/_document.js
@@ -31,16 +31,19 @@ class MyDocument extends Document {
         <Head>
           <link rel="shortcut icon" href="/vercel.svg" />
           <script type="text/javascript">
-            window.$crisp = [], window.CRISP_WEBSITE_ID =
-            "35a6cabe-2b02-430b-83f5-052512dee25d", (function (){" "}
+            if (typeof window !== "undefined"){" "}
             {
-              ((d = document),
-              (s = d.createElement("script")),
-              (s.src = "https://client.crisp.chat/l.js"),
-              (s.async = 1),
-              d.getElementsByTagName("head")[0].appendChild(s))
+              ((window.$crisp = []),
+              (window.CRISP_WEBSITE_ID =
+                "35a6cabe-2b02-430b-83f5-052512dee25d"),
+              (function () {
+                (d = document),
+                  (s = d.createElement("script")),
+                  (s.src = "https://client.crisp.chat/l.js"),
+                  (s.async = 1),
+                  d.getElementsByTagName("head")[0].appendChild(s);
+              })())
             }
-            )(),
           </script>
         </Head>
 
