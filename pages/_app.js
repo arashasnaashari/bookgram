830b79b9305510e5847d9d0ce4d1bf6410b6043a
arash <arash.asnaashari@gmail.com>
2021-04-18 21:27:38 +0430

    crips

--- pages/_app.js
+++ pages/_app.js
@@ -18,15 +18,15 @@ function MyApp({ Component, pageProps }) {
     useModel(false);
   };
   if (typeof window !== "undefined") {
-    (window.$crisp = []),
-      (window.CRISP_WEBSITE_ID = "35a6cabe-2b02-430b-83f5-052512dee25d"),
-      (function () {
-        (d = document),
-          (s = d.createElement("script")),
-          (s.src = "https://client.crisp.chat/l.js"),
-          (s.async = 1),
-          d.getElementsByTagName("head")[0].appendChild(s);
-      })();
+    window.$crisp = [];
+    window.CRISP_WEBSITE_ID = "35a6cabe-2b02-430b-83f5-052512dee25d";
+    (function () {
+      d = document;
+      s = d.createElement("script");
+      s.src = "https://client.crisp.chat/l.js";
+      s.async = 1;
+      d.getElementsByTagName("head")[0].appendChild(s);
+    })();
   }
   return (
     <>
