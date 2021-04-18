6c7faf1265ceb447cff63b0f9d41fdd5ede18b9a
arash <arash.asnaashari@gmail.com>
2021-04-18 21:19:29 +0430

    crips

--- pages/_document.js
+++ pages/_document.js
@@ -30,21 +30,6 @@ class MyDocument extends Document {
         {/* className="dark" */}
         <Head>
           <link rel="shortcut icon" href="/vercel.svg" />
-          <script type="text/javascript">
-            if(typeof window !== "undefined")
-            {
-              ((window.$crisp = []),
-              (window.CRISP_WEBSITE_ID =
-                "35a6cabe-2b02-430b-83f5-052512dee25d"),
-              (function () {
-                (d = document),
-                  (s = d.createElement("script")),
-                  (s.src = "https://client.crisp.chat/l.js"),
-                  (s.async = 1),
-                  d.getElementsByTagName("head")[0].appendChild(s);
-              })())
-            }
-          </script>
         </Head>
 
         <body className="bg-bookgram container mx-auto p-2 md:p-12 dark:bg-black overflow-x-hidden">
